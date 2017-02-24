26a0fb70db2dcc3661b8e8d6d30fc728f10871bf
Replicatte <replica.esp@outlook.com>
2017-02-24 12:00:09 +0100

    Cambios día 24/02/2017

--- initialJavaFXApplication/src/initialjavafxapplication/FXMLDocumentController.java
+++ initialJavaFXApplication/src/initialjavafxapplication/FXMLDocumentController.java
@@ -7,29 +7,47 @@ package initialjavafxapplication;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.input.KeyEvent;
 
 /**
  *
  * @author repli
  */
 public class FXMLDocumentController implements Initializable {
-    
+
     @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private Slider slide;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+
+        slide.valueProperty().addListener(new ChangeListener() {
+
+            @Override
+            public void changed(ObservableValue o, Object oldVal, Object newVal) {
+                String val = "-fx-background-color:rgb(" + newVal + "," + newVal
+                        + "," + newVal + "); -fx-font-color: red";
+
+                slide.setStyle(val);
+
+            }
+        });
+        
+        
+        slide.setOnKeyPressed((KeyEvent arg0) -> {
+            System.out.println("CLICK EN TECLA"+ arg0);
+        });
+
+        //        slide.valueProperty().addListener((observable, oldVal, newVal)
+        //                -> {
+        //            System.out.println("" + newVal);
+        //        });
+    }
+
 }
